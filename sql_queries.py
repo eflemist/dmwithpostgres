39abe5da13817fa8c40dec20cc8ec146ab235538
Ed Flemister <eflemist@gmail.com>
2021-01-17 05:39:11 -0500

    feat: added create statements

--- sql_queries.py
+++ sql_queries.py
@@ -8,19 +8,16 @@ time_table_drop = "DROP table time_dim"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay (song_title varchar, artist_name varchar, year int, album_name varchar, single varchar);""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay_fact (songplay_id int, song_id int, artist_id int, session_id int, user_id int, level varchar, location varchar, start_time varchar, user_agent varchar);""")
 
-user_table_create = ("""
-""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS user_dim (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);""")
 
-song_table_create = ("""
-""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS song_dim (song_id int, artist_id int, title varchar, year varchar, duration varchar);""")
 
-artist_table_create = ("""
-""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_dim (artist_id int, name varchar, location varchar, latitude varchar, longitude varchar);""")
 
-time_table_create = ("""
-""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time_dim (start_time varchar, year varchar, month varchar, week varchar, weakday varchar, day varchar, hour varchar);""")
+
 
 # INSERT RECORDS
 
