b8c031bb9cb67a872f399df4cf50e73e6e0dace4
Ed Flemister <eflemist@gmail.com>
2021-01-19 10:10:02 -0500

    fix: modify create statements to correct column names

--- sql_queries.py
+++ sql_queries.py
@@ -8,16 +8,15 @@ time_table_drop = "DROP TABLE IF EXISTS time_dim"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay_fact (songplay_id int, song_id int, artist_id int, session_id int, user_id int, level varchar, location varchar, start_time varchar, user_agent varchar);""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay_fact (songplay_id int, song_id varchar, artist_id varchar, session_id int, user_id int, level varchar, location varchar, start_time varchar, user_agent varchar);""")
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS user_dim (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS song_dim (song_id int, artist_id int, title varchar, year varchar, duration varchar);""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS song_dim (song_id varchar, artist_id varchar, title varchar, year varchar, duration varchar);""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_dim (artist_id int, name varchar, location varchar, latitude varchar, longitude varchar);""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_dim (artist_id varchar, artist_name varchar, artist_location varchar, artist_latitude varchar, artist_longitude varchar);""")
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time_dim (start_time varchar, year varchar, month varchar, week varchar, weakday varchar, day varchar, hour varchar);""")
-
 
 # INSERT RECORDS
 
@@ -27,8 +26,8 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
-""")
+song_table_insert = ("""INSERT INTO song_dim (artist_id, song_id, title, duration, year) VALUES (%s, %s, %s, %s, %s)""")
+
 
 artist_table_insert = ("""
 """)
